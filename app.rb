96d1ecf71bfb7012294349767e3e2156735ae5b8
ntig-lucas-ackerberg <lucas.ackerberg@elev.ga.ntig.se>
2019-02-21 11:00:42 +0100

    lektion 2, loginsystem osv
    gott godis lektion 2

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sqlite3'
 require 'slim'
+require 'bcrypt'
 enable :sessions
 
 get('/') do
@@ -11,11 +12,34 @@ get('/login') do
     slim(:login)
 end
 
-# loginpost
+post('/login') do
+    db = SQLite3::Database.new("db/db.db")
+    db.results_as_hash = true
+    password = db.execute('SELECT Lösenord FROM Användare WHERE Namn=?', params["Username"])
+    if password != []
+        if (BCrypt::Password.new(password[0][0]) == params["Password"]) == true
+            session[:username] = params["Username"]
+            redirect('/')
+        else
+            redirect('/loginfail')
+        end
+    else
+        redirect('/loginfail')
+    end
+end
 
 get('/signup') do
     slim(:signup)
 end
 
-# signuppost
+post('/signup') do
+    db = SQLite3::Database.new("db/db.db")
+    db.results_as_hash = true
+    if params["Username"] != "" && params["Password"]
+        db.execute('INSERT INTO Användare(Namn, Lösenord) VALUES (?, ?)', params["Username"], BCrypt::Password.create(params["Password"]))
+        redirect('/login')
+    else
+        redirect('/signup')
+    end
+end
 
